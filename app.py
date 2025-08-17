623137a80ee5a854ec0cdc2a416d8e7271af9264
rfelixmg <rfelixmg@gmail.com>
2025-08-17 21:23:32 +0900

    adding logger

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template_string
 import os
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -296,4 +301,6 @@ if __name__ == "__main__":
     port_env = int(os.environ.get("PORT", 8080))
     debug_env = int(os.environ.get("DEBUG", False))
 
+    if debug_env:
+        logger.info(f"Running in debug mode on port {port_env}")
     app.run(host="0.0.0.0", port=port_env, debug=debug_env)
